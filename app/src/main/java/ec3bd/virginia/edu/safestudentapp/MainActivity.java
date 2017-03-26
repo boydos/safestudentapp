ace54d3fa57fdd3ee45f25de116da39cf6a1b80d
ec3bd <ec3bd@virginia.edu>
2017-03-26 16:31:23 -0400

    this shit don't work

--- app/src/main/java/ec3bd/virginia/edu/safestudentapp/MainActivity.java
+++ app/src/main/java/ec3bd/virginia/edu/safestudentapp/MainActivity.java
@@ -1,21 +1,30 @@
 package ec3bd.virginia.edu.safestudentapp;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
 
-import com.google.zxing.Result;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+    private MapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     public void QrScanner(View view){
@@ -26,6 +35,11 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
-
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(38, -78))
+                .title("Current Location"));
+    }
 
 }
